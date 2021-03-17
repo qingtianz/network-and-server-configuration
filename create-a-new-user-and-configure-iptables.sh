c33ee9d9b44045544291f5732c93fae742102ff5
Normchou <normchou@protonmail.com>
2021-03-17 17:31:37 -0500

    Initial commit

---
+++ create-a-new-user-and-configure-iptables.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# create a new user
+sudo adduser --home /home/newuser newuser
+
+# limit internet access of the new user
+sudo iptables -A OUTPUT -m owner --uid-owner 1001 -s 192.168.0.0/24 -j ACCEPT
+sudo iptables -A OUTPUT -m owner --uid-owner 1001 -s 127.0.0.1 -j ACCEPT
+sudo iptables -A OUTPUT -m owner --uid-owner 1001 -j DROP
+sudo iptables -A INPUT -m owner --uid-owner 1001 -s 192.168.0.0/24 -j ACCEPT
+sudo iptables -A INPUT -m owner --uid-owner 1001 -s 127.0.0.1 -j ACCEPT
+sudo iptables -A INPUT -m owner --uid-owner 1001 -j DROP
+
+# save configuration
+sudo iptables-save > /etc/iptables.conf
+
+# use saved configuration after reboot
+sudo iptables-restore < /etc/iptables.conf
